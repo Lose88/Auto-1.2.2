4d5a7cd09e841ff7902957ea52f3eb880065ce75
Lose88 <hotsting1888@gmail.com>
2022-09-10 20:36:29 +0300

    initCommit1

---
+++ src/test/java/TestEcho.java
@@ -0,0 +1,24 @@
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+public class TestEcho {
+
+
+    @Test
+    void postTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hello! World!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello, World"));
+    }
+}
